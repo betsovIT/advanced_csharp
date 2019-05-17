b253dd00cd4b59a9af4f23b6e666a88e17d111ae
Dimitar Betsov <d.betsov@students.softuni.bg>
2019-05-17 20:51:37 +0300

    Solved the last problem

--- Multidimensional Arrays/Pascal Triangle/Program.cs
+++ Multidimensional Arrays/Pascal Triangle/Program.cs
@@ -6,7 +6,25 @@ namespace Pascal_Triangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int size = int.Parse(Console.ReadLine());
+            long[][] pascalTriangle = new long[size][];
+            pascalTriangle[0] = new long[] { 1 };
+
+            for (int i = 1; i < pascalTriangle.Length; i++)
+            {
+                pascalTriangle[i] = new long[i + 1];
+                pascalTriangle[i][0] = 1;
+                for (int j = 1; j < pascalTriangle[i].Length-1; j++)
+                {
+                    pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+                }
+                pascalTriangle[i][pascalTriangle[i].Length - 1] = 1;
+            }
+
+            for (int i = 0; i < pascalTriangle.Length; i++)
+            {
+                Console.WriteLine(string.Join(' ',pascalTriangle[i]));
+            }
         }
     }
 }
