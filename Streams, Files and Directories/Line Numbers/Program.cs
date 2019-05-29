88f5ece8339a6e6713018a28b9fc20487d419d87
betsovIT <d.betsov@gmail.com>
2019-05-29 11:57:08 +0300

    Solved some labs

---
+++ Streams, Files and Directories/Line Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "files";
+            string fileNameInput = "Input.txt";
+            string fileNameOutput = "Output.txt";
+            string filePathInput = Path.Combine(path, fileNameInput);
+            string filePathOutput = Path.Combine(path, fileNameOutput);
+            int counter = 1;
+
+            using (var reader = new StreamReader(filePathInput))
+            {
+                using (var writer = new StreamWriter(filePathOutput))
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {                        
+                        writer.WriteLine($"{counter}. " + line);
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                    
+                }
+            }
+        }
+    }
+}
