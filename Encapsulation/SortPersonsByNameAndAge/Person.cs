fc5833e3001469edcf80e4c91c86e6bebf13dc73
Dimitar Betsov <d.betsov@gmail.com>
2019-07-05 20:54:33 +0300

    Solved all of the labs.

---
+++ Encapsulation/SortPersonsByNameAndAge/Person.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            set
+            {
+                this.salary = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public void IncreaseSalary(decimal percent)
+        {
+            if (this.Age >= 30)
+            {
+                salary = salary * (1 + percent / 100);
+            }
+            else
+            {
+                salary = salary * (1 + percent / 200);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} recieves {Salary:F2} leva.";
+        }
+
+    }
+}
---
+++ Encapsulation/SortPersonsByNameAndAge/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int entries = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < entries; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                var person = new Person(firstName, lastName, age);
+
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
